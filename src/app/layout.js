aa9c7df675ea79409f35e3314505f78bfe59976a
sigidhanafi <sigidhanafi@gmail.com>
2025-02-10 12:30:46 +0700

    move base ui to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -22,7 +23,36 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className='min-h-screen'>
+          {/* Navbar */}
+          <nav className='fixed top-0 left-0 right-0 bg-white flex px-6 py-4 border-b shadow-sm z-50'>
+            {/* <nav className='flex bg-white p-4 shadow-sm items-center justify-center'> */}
+            <div className='flex flex-row mx-auto w-3/5 justify-between'>
+              <div className='text-xl font-semibold'>DSS Framework</div>
+              <div className='space-x-6 text-gray-600'>
+                <Link href='#' className='hover:text-blue-500'>
+                  Home
+                </Link>
+                <Link href='#' className='hover:text-blue-500'>
+                  Explore Topic
+                </Link>
+                <Link href='#' className='hover:text-blue-500'>
+                  How it Work
+                </Link>
+                <Link href='#' className='hover:text-blue-500'>
+                  Login
+                </Link>
+              </div>
+            </div>
+          </nav>
+
+          {children}
+
+          {/* Footer */}
+          <footer className='bg-blue-900 text-white text-center py-4 mt-auto'>
+            <p>copyright @2025 Tugas Kuliah DSS</p>
+          </footer>
+        </div>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,29 +3,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='min-h-screen'>
-      {/* Navbar */}
-      <nav className='fixed top-0 left-0 right-0 bg-white flex px-6 py-4 border-b shadow-sm z-50'>
-        {/* <nav className='flex bg-white p-4 shadow-sm items-center justify-center'> */}
-        <div className='flex flex-row mx-auto w-3/5 justify-between'>
-          <div className='text-xl font-semibold'>DSS Framework</div>
-          <div className='space-x-6 text-gray-600'>
-            <a href='#' className='hover:text-blue-500'>
-              Home
-            </a>
-            <a href='#' className='hover:text-blue-500'>
-              Explore Topic
-            </a>
-            <a href='#' className='hover:text-blue-500'>
-              How it Work
-            </a>
-            <a href='#' className='hover:text-blue-500'>
-              Login
-            </a>
-          </div>
-        </div>
-      </nav>
-
+    <>
       {/* Main Content */}
       <div className='flex flex-col items-center justify-center min-h-60'>
         <h1 className='text-3xl font-bold'>DSS Framework</h1>
@@ -80,11 +58,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      {/* Footer */}
-      <footer className='bg-blue-900 text-white text-center py-4 mt-auto'>
-        <p>copyright @2025 Tugas Kuliah</p>
-      </footer>
-    </div>
+    </>
   );
 }
